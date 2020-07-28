e5de02aaaf4060df963f9659d005964ace2e8c5f
Suraj Samaga <surajsamaga@gmail.com>
2020-07-28 23:45:55 +0530

    Changed nav-bar, made video responsive, removed timetable, changed color scheme

--- docs/appl.js
+++ docs/appl.js
@@ -1,79 +1,59 @@
-// nav bar slider 
-const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-content');
-	const navItems = document.querySelectorAll('.nav-content li');
-	
-	// on click do something
-	burger.addEventListener('click', () => {
-		// toggle to nav-active class to show the <ul>
-		nav.classList.toggle('nav-active');
-		
-		// animate the links
-		navItems.forEach((link, index) => {
-			if(link.style.animation){
-				link.style.animation = '';
-			}
-			else{
-				link.style.animation = `navFade 0.5s ease forwards ${index/5 + 0.5}s`;
-			}
-		});
-		burger.classList.toggle('clickburger');
-	});
-}
+// navbar on mobile devices:
+function navSlide(){
+	const hamburger = document.querySelector('.hamburger');
+	const navLinks = document.querySelector('.nav-links');
+	const links = document.querySelectorAll('.nav-links li');
 
-// scroll to top button
-function btnscroll(){
-	const btnscroll = document.querySelector("#topbtn");
-	btnscroll.addEventListener("click", function(){
-		window.scrollTo({
-			top: 0,
-			left: 0,
-			behavior: "smooth"
+
+	hamburger.addEventListener('click', () =>{
+		navLinks.classList.toggle('open');
+		links.forEach(link =>{
+			link.classList.toggle('fade');
 		});
 	});
 };
 
-// nav bar style change
-function navScroll(){
-	let mainNavLinks = document.querySelectorAll("nav ul li a");
-	let mainSections = document.querySelectorAll(".overlay");
-
-	let lastId;
-	let cur = [];
-	
-	window.addEventListener("scroll", event => {
-		let fromTop = window.scrollY;
-		mainNavLinks.forEach(link => {
-			let section = document.querySelector(link.hash);
-
-			if(section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop){
-				link.classList.add("current");
-			}
-			else{
-				link.classList.remove("current");
-			}
-			
-		});
-	});
-};
-
-// downbtn scroll
-function downbtnScroll(){
-	const downbtn = document.querySelector("#downbtn");
-	const scrollMarker = document.querySelector("#About");
-	const targetTop = scrollMarker.offsetHeight;
-	downbtn.addEventListener("click", function(){
+function topbtnScroll(){
+	const topbtn = document.querySelector('#topbtn');
+	topbtn.addEventListener('click', function(){
 		window.scrollTo({
-			top:targetTop,
+			top:0,
 			left:0,
-			behavior:"smooth"
+			behavior:'smooth'
 		});
 	});
 };
 
+function downbtnScroll(){
+	const downbtn = document.querySelector('#downbtn');
+	const target = document.querySelector('#About');
+
+	downbtn.addEventListener('click', function(){
+		window.scrollTo({
+			top:target.offsetHeight,
+			left:0,
+			behavior:'smooth'
+		});
+	});
+};
+
+function burgerAnimate(){
+    const burger = document.querySelector('.hamburger');
+    let open =false;
+    burger.addEventListener('click', () => {
+        if(!open){
+            burger.classList.add('open');
+            open = true;
+        }
+        else{
+            burger.classList.remove('open');
+            open = false;
+        }
+    });
+
+};
 
 navSlide();
-btnscroll();
-navScroll();
+topbtnScroll();
 downbtnScroll();
+burgerAnimate();
